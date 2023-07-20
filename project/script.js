8f67071942d55e2fb620e1273e9efcd1f7ebaf20
ᴢᴇʏʀᴏx <97727454+devil624@users.noreply.github.com>
2023-07-20 22:42:56 +0400

    Create script.js

---
+++ project/script.js
@@ -0,0 +1,33 @@
+let string = "";
+let buttons= document.querySelectorAll('.btn');
+Array.from(buttons).forEach((btn)=>{
+  btn.addEventListener('click',(ev)=>{
+    if(ev.target.innerHTML == '=')
+    {
+      string=eval(string);
+      document.querySelector('#input').value = string;
+    }
+      else if(ev.target.innerHTML == 'C')
+    {
+      string="";
+      document.querySelector('#input').value = string;
+    }
+        else if(ev.target.innerHTML == 'backspace')
+    {
+      string= string.slice(0,-1);
+      document.querySelector('#input').value = string;
+    }
+    else if(ev.target.innerHTML == 'x')
+    {
+      string= string +'*';
+      document.querySelector('#input').value = string;
+    }
+      else 
+    {
+    console.log(ev.target);
+    string= string + ev.target.innerHTML;
+    console.log(string)
+    document.querySelector('#input').value = string}
+  })
+  
+}) 
