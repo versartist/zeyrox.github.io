5848d7de2a6bc956658caf722a99dad44044d4b8
ᴢᴇʏʀᴏx <97727454+versartist@users.noreply.github.com>
2023-10-22 16:43:34 +0400

    fix

--- projects/codewell chlng/webpage/script.js
+++ projects/codewell chlng/webpage/script.js
@@ -1,7 +1,9 @@
 function showSide() {
     document.querySelector('.nav-card').style.display='block';
+    document.querySelector('.hamb').style.display='none';
 
 }
 function hideSide() {
+    document.querySelector('.hamb').style.display='block';
     document.querySelector('.nav-card').style.display='none';
 }
